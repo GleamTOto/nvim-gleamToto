42623d1e9432f5f8eb3fa7b76466635cca7b294a
GleamTOto <cristian.bohorquez@uniminuto.edu.co>
2025-02-25 08:37:39 -0500

    Readme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  opts = {
+    transparent_background = true, -- Habilitar fondo transparente
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      telescope = true,
+      which_key = true,
+    },
+  },
+}
