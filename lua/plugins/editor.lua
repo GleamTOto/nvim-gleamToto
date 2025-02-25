8819d673e248dcd9788ce3d0d768d48c72403861
GleamTOto <cristian.bohorquez@uniminuto.edu.co>
2025-02-25 17:22:15 -0500

    comment add languages

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -2,16 +2,80 @@ return {
   {
     "folke/ts-comments.nvim",
     requires = { "nvim-treesitter/nvim-treesitter" },
-    keymaps = {
-      comment = "gc",
-      comment_line = "gcc",
-      textobject = "gc",
+    opts = {
+      lang = {
+        astro = "<!-- %s -->",
+        axaml = "<!-- %s -->",
+        blueprint = "// %s",
+        c = "// %s",
+        c_sharp = "// %s",
+        clojure = { ";; %s", "; %s" },
+        cpp = "// %s",
+        cs_project = "<!-- %s -->",
+        cue = "// %s",
+        fsharp = "// %s",
+        fsharp_project = "<!-- %s -->",
+        gleam = "// %s",
+        glimmer = "{{! %s }}",
+        graphql = "# %s",
+        handlebars = "{{! %s }}",
+        hcl = "# %s",
+        html = "<!-- %s -->",
+        hyprlang = "# %s",
+        ini = "; %s",
+        ipynb = "# %s",
+        javascript = {
+          "// %s", -- default commentstring when no treesitter node matches
+          "/* %s */",
+          call_expression = "// %s", -- specific commentstring for call_expression
+          jsx_attribute = "// %s",
+          jsx_element = "{/* %s */}",
+          jsx_fragment = "{/* %s */}",
+          spread_element = "// %s",
+          statement_block = "// %s",
+        },
+        kdl = "// %s",
+        php = "// %s",
+        rego = "# %s",
+        rescript = "// %s",
+        rust = { "// %s", "/* %s */" },
+        sql = "-- %s",
+        styled = "/* %s */",
+        svelte = "<!-- %s -->",
+        templ = {
+          "// %s",
+          component_block = "<!-- %s -->",
+        },
+        terraform = "# %s",
+        tsx = {
+          "// %s", -- default commentstring when no treesitter node matches
+          "/* %s */",
+          call_expression = "// %s", -- specific commentstring for call_expression
+          jsx_attribute = "// %s",
+          jsx_element = "{/* %s */}",
+          jsx_fragment = "{/* %s */}",
+          spread_element = "// %s",
+          statement_block = "// %s",
+        },
+        twig = "{# %s #}",
+        typescript = { "// %s", "/* %s */" }, -- langs can have multiple commentstrings
+        vue = "<!-- %s -->",
+        xaml = "<!-- %s -->",
+      },
+      mappings = {
+        comment = "gc",
+        motion = "gcc",
+        textobject = "gc",
+      },
     },
+    config = function(_, opts)
+      require("ts-comments").setup(opts)
+    end,
   },
   {
     enabled = false,
     "folke/flash.nvim",
-    --@type Flash.Config
+    ---@type Flash.Config
     opts = {
       search = {
         forward = true,
